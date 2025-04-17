f092f026d9d5ff88d8ebb75cdd4f27b64e82de31
BoxinCao <ngxmex@gmail.com>
2025-04-17 02:35:20 -0400

    updated

--- express_server.js
+++ express_server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+
+function generateRandomString() {
+  return Math.random().toString(36).slice(2,8);
+}
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,24 +28,34 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
+// app.get("/set", (req, res) => {
+//   const a = 1;
+//   res.send(`a = ${a}`);
+// });
 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
+// app.get("/fetch", (req, res) => {
+//   res.send(`a = ${a}`);
+// });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; 
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const templateVars = { id: id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
+});
+
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
